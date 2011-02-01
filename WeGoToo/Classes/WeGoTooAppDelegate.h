2a32a78a0ed8b8e921e01bdf3a49fe340cbc674b
Jason Leong <ruzaus-mail@yahoo.com.au>
2011-02-01 18:15:01 +1000

    Blank iOS Window-based Application with Core Data

---
+++ WeGoToo/Classes/WeGoTooAppDelegate.h
@@ -0,0 +1,32 @@
+//
+//  WeGoTooAppDelegate.h
+//  WeGoToo
+//
+//  Created by Dian Tjondronegoro on 1/02/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface WeGoTooAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
